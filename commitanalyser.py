205f372e65b479cac73851c0883a8e284fc5c70e
Berke Diler <76554589+bercats@users.noreply.github.com>
2023-11-05 22:44:39 +0200

    Update commitanalyser.py

--- commitanalyser.py
+++ commitanalyser.py
@@ -2,10 +2,14 @@ import sys
 import re
 import matplotlib.pyplot as plt
 
+swmFeatures = ["Adaptive", "Corrective", "Perfective"]
+nfrFeatures = ["Maintainability", "Usability", "Functionality", "Reliability", "Efficiency", "Portability"]
+softEvolFeatures = ["Forward Engineering", "Re-Engineering", "Corrective Engineering", "Management"]
+classificationSchemes = ["SwM tasks", "NFR Labeling", "SoftEvol tasks"]
 
 def parse_identities(identities_txt):
+    identities_dict = {}
     try:
-        identities_dict = {}
         file = open(identities_txt, "r")
         for line in file:
             if line.startswith("Commiter ID"):
@@ -16,20 +20,15 @@ def parse_identities(identities_txt):
                 name = line_data[1]
                 email = line_data[2].strip()
                 # Create a new person dictionary if the committer ID is not in the dictionary
-                if committer_id not in identities_dict:
-                    identities_dict[committer_id] = {
-                        "name": name,
-                        "email": email,
-                    }
+                identities_dict[committer_id] = {"name": name, "email": email}
         return identities_dict
     except IOError:
         print("Could not open file " + identities_txt)
         return {}
 
-
 def parse_commits(commit_txt, identities_dict):
+    commit_dict = {}
     try:
-        commit_dict = {}
         file = open(commit_txt, "r")
         for line in file:
             if line.startswith("Commit ID"):
@@ -88,148 +87,88 @@ def menu(commit_txt, identities_txt):
             opt1(commit_dict)
             print("\n")
         elif choice == 2:
-            opt2(commit_dict)
+            combined_opt(commit_dict, choice)
             print("\n")
         elif choice == 3:
-            opt3(commit_dict)
+            combined_opt(commit_dict, choice)
             print("\n")
     return
-
+    
 def opt1(commit_dict):
+    names = list(commit_dict.keys())
+    labels = None
     try:
         print("Select a developer: ")
-        for name in commit_dict:
-            print(name)
-        name = input("Enter your choice: ")
+        for i, name in enumerate(commit_dict,1):
+            print(f"{i}. {name}")
+        name = names[int(input("Enter your choice: ")) - 1]
         print("\n")
-        print("Select a classification scheme: ")
-        print("1. SwM tasks")
-        print("2. NFR Labeling")
-        print("3. SoftEvol tasks")
+        print("Select a classification scheme:")
+        for i, scheme in enumerate(classificationSchemes,1):
+            print(f"{i}. {scheme}")
         scheme = int(input("Enter your choice: "))
         print("\n")
         if scheme == 1:
-            labels = ["Adaptive", "Corrective", "Perfective"]
-            values = [commit_dict[name]["SwM tasks"][0], commit_dict[name]["SwM tasks"][1],commit_dict[name]["SwM tasks"][2]]
-            plot_graph(f"Comparison of {name}'s commits for SwM tasks", labels, values)
+            labels = swmFeatures
         elif scheme == 2:
-            labels = ["Maintainability", "Usability", "Functionality", "Reliability", "Efficiency", "Portability"]
-            values = [commit_dict[name]["NFR Labeling"][0], commit_dict[name]["NFR Labeling"][1],
-                      commit_dict[name]["NFR Labeling"][2], commit_dict[name]["NFR Labeling"][3],
-                      commit_dict[name]["NFR Labeling"][4], commit_dict[name]["NFR Labeling"][5]]
-            plot_graph(f"Comparison of {name}'s commits for NFR Labeling", labels, values)
+            labels = nfrFeatures
         elif scheme == 3:
-            labels = ["Forward Engineering", "Re-Engineering", "Corrective Engineering", "Management"]
-            values = [commit_dict[name]["SoftEvol tasks"][0], commit_dict[name]["SoftEvol tasks"][1],
-                      commit_dict[name]["SoftEvol tasks"][2], commit_dict[name]["SoftEvol tasks"][3]]
-            plot_graph(f"Comparison of {name}'s commits for SoftEvol tasks", labels, values)
+            labels = softEvolFeatures
         else:
             print("Invalid choice.")
+        values = commit_dict[name][classificationSchemes[scheme - 1]]
+        plot_graph(f"Comparison of {name}'s commits for {classificationSchemes[scheme-1]}", labels, values)
     except KeyError:
         print("Invalid name.")
     return
 
-
-def opt2(commit_dict):
+def combined_opt(commit_dict, option):
     try:
         print("Select a classification scheme:")
-        print("1. SwM tasks")
-        print("2. NFR Labeling")
-        print("3. SoftEvol tasks")
+        for i, scheme in enumerate(classificationSchemes, 1):
+            print(f"{i}. {scheme}")
         scheme = int(input("Enter your choice: "))
         print("\n")
-        scheme_str = ""
         if scheme == 1:
-            scheme_str = "SwM tasks"
-            print("SwM tasks:")
-            print("Select a feature:")
-            feature = int(input("1. Adaptive Tasks\n2. Corrective Tasks\n3. Perfective Tasks\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Adaptive Tasks", "Corrective Tasks", "Perfective Tasks"]
+            feature_names = swmFeatures
         elif scheme == 2:
-            scheme_str = "NFR Labeling"
-            print("NFR Labeling:")
-            print("Select a feature:")
-            feature = int(input("1. Maintainability\n2. Usability\n3. Functionality\n4. Reliability\n5. Efficiency\n6. Portability\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Maintainability", "Usability", "Functionality", "Reliability", "Efficiency", "Portability"]
+            feature_names = nfrFeatures
         elif scheme == 3:
-            scheme_str = "SoftEvol tasks"
-            print("SoftEvol tasks:")
-            print("Select a feature:")
-            feature = int(input("1. Forward Engineering\n2. Re-Engineering\n3. Corrective Engineering\n4. Management\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Forward Engineering", "Re-Engineering", "Corrective Engineering", "Management"]
+            feature_names = softEvolFeatures
         else:
             print("Invalid choice.")
             return
 
-        if feature in range(1, len(feature_names) + 1):
-            feature_name = feature_names[feature - 1]
-            labels = []
-            values = []
-            for name in commit_dict:
-                labels.append(name)
-                values.append(commit_dict[name][scheme_str][feature - 1])
-            plot_graph(f"Comparison of commits for {feature_name}", labels, values)
-        else:
-            print("Invalid choice.")
-
-    except KeyError:
-        print("Invalid name.")
-    return
-
-def opt3(commit_dict):
-    try:
-        print("Select a classification scheme: ")
-        print("1. SwM tasks")
-        print("2. NFR Labeling")
-        print("3. SoftEvol tasks")
-        scheme = int(input("Enter your choice: "))
+        print("Select a feature:")
+        for i, feature in enumerate(feature_names, 1):
+            print(f"{i}. {feature}")
+        feature = int(input("Enter your choice: "))
         print("\n")
-        scheme_str = ""
-        if scheme == 1:
-            scheme_str = "SwM tasks"
-            print("SwM tasks:")
-            print("Select a feature:")
-            feature = int(input("1. Adaptive Tasks\n2. Corrective Tasks\n3. Perfective Tasks\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Adaptive Tasks", "Corrective Tasks", "Perfective Tasks"]
-        elif scheme == 2:
-            scheme_str = "NFR Labeling"
-            print("NFR Labeling:")
-            print("Select a feature:")
-            feature = int(input(
-                "1. Maintainability\n2. Usability\n3. Functionality\n4. Reliability\n5. Efficiency\n6. Portability\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Maintainability", "Usability", "Functionality", "Reliability", "Efficiency", "Portability"]
-        elif scheme == 3:
-            scheme_str = "SoftEvol tasks"
-            print("SoftEvol tasks:")
-            print("Select a feature:")
-            feature = int(input(
-                "1. Forward Engineering\n2. Re-Engineering\n3. Corrective Engineering\n4. Management\nEnter your choice: "))
-            print("\n")
-            feature_names = ["Forward Engineering", "Re-Engineering", "Corrective Engineering", "Management"]
-        else:
-            print("Invalid choice.")
-            return
 
         if feature in range(1, len(feature_names) + 1):
             feature_name = feature_names[feature - 1]
-            max_commits = 0
-            max_name = ""
-            for name in commit_dict:
-                if commit_dict[name][scheme_str][feature - 1] > max_commits:
-                    max_commits = commit_dict[name][scheme_str][feature - 1]
-                    max_name = name
-            print(f"The developer with the maximum number of commits for {feature_name} is {max_name}: {max_commits}")
+
+            if option == 2:
+                labels = list(commit_dict.keys())
+                values = []
+                for name in commit_dict:
+                    values.append(commit_dict[name][classificationSchemes[scheme - 1]][feature - 1])
+                plot_graph(f"Comparison of commits for {feature_name}", labels, values)
+
+            elif option == 3:
+                max_commits = 0
+                max_developer = ""
+                for name in commit_dict:
+                    if commit_dict[name][classificationSchemes[scheme - 1]][feature - 1] > max_commits:
+                        max_commits = commit_dict[name][classificationSchemes[scheme - 1]][feature - 1]
+                        max_developer = name
+                print(f"{max_developer} has {max_commits} commits classified by {feature_name}")
+            else:
+                print("Invalid option.")
         else:
             print("Invalid choice.")
     except KeyError:
         print("Invalid name.")
-    return
-
 
 def main():
     if len(sys.argv) != 3:
@@ -239,7 +178,6 @@ def main():
     commit_txt = sys.argv[1]
     identities_txt = sys.argv[2]
     menu(commit_txt, identities_txt)
-
 
 if __name__ == '__main__':
     main()
